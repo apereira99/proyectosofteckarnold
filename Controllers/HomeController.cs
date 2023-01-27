368fe686a09b29896927a62e5f377b8ff62d6ee8
apereira99 <30730139+apereira99@users.noreply.github.com>
2023-01-27 18:40:14 -0500

    ultimo cambio

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Proyecto_Arnold.DAL;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,11 +9,28 @@ namespace Proyecto_Arnold.Controllers
 {
     public class HomeController : Controller
     {
+        private ListarProducto _listarProducto;
+
+        public HomeController()
+        {
+            _listarProducto = new ListarProducto();
+        }
         public ActionResult Index()
         {
-            ViewBag.Title = "Home Page";
-
             return View();
         }
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page";
+            return View();
+        }
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page";
+            var model = _listarProducto.ObtenerProductos();
+            return View(model);
+
+        }
+
     }
 }
---
+++ DAL/ListarProducto.cs
@@ -0,0 +1,24 @@
+﻿using Proyecto_Arnold.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Proyecto_Arnold.DAL
+{
+    public class ListarProducto
+    {
+        public List<Producto> ObtenerProductos()
+        {
+
+            return new List<Producto>()
+            {
+                new Producto()
+                {
+                    NombreProducto = "Laptop HP Core i5",
+                    Descripcion = "Laptop Ultima Generacion"
+                },
+            };
+        }
+    }
+}
